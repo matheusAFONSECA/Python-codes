2f651b2817d0f7a45083e29d84330c6fb76b6faa
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-03-09 17:36:30 -0300

    Código para trocar uma string em um aquivo .txt

--- Detecção_de_placas/chat3.py
+++ Detecção_de_placas/chat3.py
@@ -1,11 +1,13 @@
 # código que apaga um sequencia de String em um aquivo .txt
-with open('arquivo.txt', 'r') as f:
-    linhas = f.readlines()
 
-linhas_sem_repeticao = []
-for linha in linhas:
-    if linha not in linhas_sem_repeticao:
-        linhas_sem_repeticao.append(linha)
+# Abrir arquivo e ler conteúdo
+with open("C:\\Users\\mathe\\Downloads\\nomes_dos_arquivos - Copia.txt", 'r') as f:
+    conteudo = f.read()
 
-with open('arquivo_sem_repeticao.txt', 'w') as f:
-    f.writelines(linhas_sem_repeticao)
+# Remover sequência de string desejada
+sequencia = "'"
+novo_conteudo = conteudo.replace(sequencia, '')
+
+# Escrever novo conteúdo no mesmo arquivo
+with open("C:\\Users\\mathe\\Downloads\\nomes_dos_arquivos - Copia.txt", 'w') as f:
+    f.write(novo_conteudo)
